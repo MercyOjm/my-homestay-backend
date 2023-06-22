243ca395da808f6eb500b604af11bf3107576042
MercyOjm <mercyojiemudia3@gmail.com>
2023-06-22 10:53:17 +0200

    feature-hom-66

--- routes/property.route.js
+++ routes/property.route.js
@@ -1,6 +1,7 @@
 /* eslint-disable import/no-named-as-default-member */
 import express from 'express';
 import { createProperty, deleteProperty, getAllProperty, getPropertyDetail, getPropertyPreview, searchProperties, getPropertyByBookingHistory, updateProperty } from '../controllers/property.controller.js';
+import { propertyValidation } from '../validations/property.js';
 
 const propertyRoutes = express.Router(); // eslint-disable-line new-cap
 propertyRoutes.post('/', createProperty)
@@ -9,8 +10,8 @@ propertyRoutes.post('/', createProperty)
       .get('/:id', getPropertyDetail)
       .delete('/:id', deleteProperty)
       .get('/:propertyId/preview', getPropertyPreview)
-      .get('/:id/booking-history', getPropertyByBookingHistory)
-      .put('/:id', updateProperty);
+      .get('/:id/booking-history',protect(), validate(propertyValidation), getPropertyByBookingHistory)
+      .put('/:id',protect(), validate(propertyValidation), updateProperty);
 
 
       
