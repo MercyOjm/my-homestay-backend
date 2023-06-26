aa58517053881584b92e27b14351c51aaca39790
triplnj <ivkovicd69@gmail.com>
2023-06-26 09:34:53 +0200

    small changes

--- controllers/property.controller.js
+++ controllers/property.controller.js
@@ -1,69 +1,66 @@
-import Property from '../models/property.model.js';
-  
-export const createProperty = async (req, res, next) => {
-    const propertyData = req.body;
-    try {
-   
-  
-      // Create a new property object using the Property model
-      const property = new Property(propertyData);
-  
-      // Save the property to the database
-      await property.save();
-  
-      res.status(201).json({ message: 'Property created successfully', property });
-    } catch (error) {
-      next(error);
-    }
-  }
-  
+import Property from "../models/property.model.js";
 
+export const createProperty = async (req, res, next) => {
+  const propertyData = req.body;
+  try {
+    // Create a new property object using the Property model
+    const property = new Property(propertyData);
+
+    // Save the property to the database
+    await property.save();
+
+    res
+      .status(201)
+      .json({ message: "Property created successfully", property });
+  } catch (error) {
+    next(error);
+  }
+};
 
 export const getPropertyDetail = async (req, res, next) => {
-    const propertyId = req.params.id
-    try {
-      const property = await Property.findById(propertyId)
-        // .populate('images', '-_id') 
-        // .populate('host', 'email')
-        // .populate('address', 'street city') 
-        // .populate('reviews', '-_id') 
-        // .select('-booked_dates'); 
-  
-      if (!property) {
-        return res.status(404).json({ error: 'Property not found' });
-      }
-  
-      res.status(200).json(property);
-    } catch (error) {
-      next(error)
-    }
-  };
-  
-export const deleteProperty = async (req, res, next) => {
-    const propertyId = req.params.id
-    try {
-      const deletedProp = await Property.findByIdAndDelete(propertyId)
-        
-      if (!deletedProp) {
-        return res.status(404).json({ error: 'Property not found' });
-      }
-  
-      res.status(200).json({message: 'Property deleted.', deletedProp});
-    } catch (error) {
-      next(error)
-    }
-  };
+  const propertyId = req.params.id;
+  try {
+    const property = await Property.findById(propertyId);
+    // .populate('images', '-_id')
+    // .populate('host', 'email')
+    // .populate('address', 'street city')
+    // .populate('reviews', '-_id')
+    // .select('-booked_dates');
 
-  export const getAllProperty = async(req, res, next) => {
-    try {
-      const properties = await Property.find()
-      if(!properties){
-        return res.status(404).json({message: 'No properties found.'})
-      }
-
-      res.status(200).json(properties)
-      
-    } catch (error) {
-      next(error)
+    if (!property) {
+      return res.status(404).json({ error: "Property not found" });
     }
+
+    res.status(200).json(property);
+  } catch (error) {
+    next(error);
   }
+};
+
+export const deleteProperty = async (req, res, next) => {
+  const propertyId = req.params.id;
+  try {
+    const deletedProp = await Property.findByIdAndDelete(propertyId);
+
+    if (!deletedProp) {
+      return res.status(404).json({ error: "Property not found" });
+    }
+
+    res.status(200).json({ message: "Property deleted.", deletedProp });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getAllProperty = async (req, res, next) => {
+  try {
+    const properties = await Property.find();
+    if (!properties) {
+      return res.status(404).json({ message: "No properties found." });
+    }
+
+    res.status(200).json(properties);
+  } catch (error) {
+    next(error);
+  }
+};
