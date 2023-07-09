e9bbf782c11d776d6ffc0c54b717cda267955d29
shraddhamhatre113 <shraddhamhatre113@gmail.com>
2023-07-10 01:11:41 +0200

    change

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,18 +1,14 @@
 import express from 'express';
 import { signin, signup } from '../controllers/user.controller.js';
 
-import { createProperty } from '../controllers/property.controller.js';
-
-import { protect } from '../middleware/auth.js';
-
 import { signInValidation, userValidation } from '../validations/user.js';
 
-const userRoutes = express.Router(); // eslint-disable-line new-cap
+const router = express.Router(); // eslint-disable-line new-cap
 
 router.route('/signup')
   .post(validator(userValidation), signup)
 
-userRoutes.route('/signin').post(signin);
+  router.route('/signin').post(signin);
 
 router.route('/signin')
   .post(validator(signInValidation),  signin)
